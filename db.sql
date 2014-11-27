b16456332e1a3f349e88152885e0c874ac69e19e
rydeen <pedro_abreu12@hotmail.com>
2014-11-27 19:05:50 +0000

    ficheiro de texto com informações e código sql ralativos às tabelas que nos interessam do ERP

---
+++ db.sql
@@ -0,0 +1,44 @@
+-- 		############################################################################
+-- 		### ------------ ### ---------- FORNECEDORES ---------- ### ------------ ###
+-- 		############################################################################
+-- 		Nome da tabela: dbo.Fornecedores
+			select * from PRISINF.dbo.Fornecedores ;
+			
+-- 		Atributos relevantes
+--			(PK) Fornecedore : string > id do fornecedor :: por exemplo, "F0001"
+--			(--) Nome : string > nome do fornecedor
+--			(--) NumContrib : int > número de contribuinte
+--			(--) EncomendasPendentes : float > número de encomendas pendentes ?? 
+--			(--) NomeFiscal : string > nome fiscal do fornecedor
+
+-- 		############################################################################
+-- 		### ------------ ### ------------ ARMAZENS ------------ ### ------------ ###
+-- 		############################################################################
+-- 		Nome da tabela: dbo.Armazem
+			select * from PRISINF.dbo.Armazem ;
+	
+-- 		Atributos relevantes
+--			(PK) Armazem : string > id do armazem
+--			(--) Descricao : string > descrição/nome/identificação do armazém
+--			(FK) Pais : Armazens_Pais
+--			(FK) Distrito : Armazens_Distritos 
+		
+-- 		############################################################################
+-- 		### ------------ ### ------------- PAISES ------------- ### ------------ ###
+-- 		############################################################################
+-- 		Nome da tabela: dbo.Paises
+			select * from PRISINF.dbo.Paises ;
+	
+-- 		Atributos relevantes
+--			(PK) Pais : string > id do pais :: por exemplo, "PT"
+--			(--) Descricao : string > nome do pais :: por exemplo, "Portugal"
+		
+-- 		############################################################################
+-- 		### ------------ ### ------------- PAISES ------------- ### ------------ ###
+-- 		############################################################################
+-- 		Nome da tabela: dbo.Distritos
+			select * from PRISINF.dbo.Distritos ;
+	
+-- 		Atributos relevantes
+--			(PK) Distrito : string > id do distrito :: por exemplo, "01"
+--			(--) Descricao : string > nome do distrito :: por exemplo, "Aveiro"
